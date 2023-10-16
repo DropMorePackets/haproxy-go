c30cc64cefecc38e864d46b31dacff5f6c0a32f7
Fionera <fionera@fionera.de>
2023-10-16 04:29:10 +0200

    fix allocation tests

--- pkg/testutil/allocs.go
+++ pkg/testutil/allocs.go
@@ -1,32 +1,28 @@
 package testutil
 
 import (
-	"runtime"
 	"testing"
 )
+
+const runsPerTest = 10
 
 func WithoutAllocations(fn func(*testing.T)) func(t *testing.T) {
 	return WithNAllocations(0, fn)
 }
 
 func WithNAllocations(n uint64, fn func(*testing.T)) func(t *testing.T) {
-	var m runtime.MemStats
 	return func(t *testing.T) {
-		runtime.ReadMemStats(&m)
-		prev := m.TotalAlloc
-
-		fn(t)
+		avg := testing.AllocsPerRun(runsPerTest, func() {
+			fn(t)
+		})
 
 		// early exit when failed
 		if t.Failed() {
 			return
 		}
 
-		runtime.ReadMemStats(&m)
-		after := m.TotalAlloc
-
-		if after-prev != n {
-			t.Errorf("%d bytes got allocated, should be %d", after-prev, n)
+		if uint64(avg) != n {
+			t.Errorf("got %v allocs, want %d allocs", avg, n)
 		}
 	}
 }
--- pkg/testutil/allocs_test.go
+++ pkg/testutil/allocs_test.go
@@ -2,16 +2,16 @@ package testutil
 
 import (
 	"testing"
-	"unsafe"
 )
 
 func TestAllocTests(t *testing.T) {
-	v := make([]byte, 10)
 	t.Run("without allocations", WithoutAllocations(func(t *testing.T) {
+		v := make([]byte, 10)
 		copy(v, []byte{1, 2, 3, 4})
 	}))
 
-	t.Run("with allocation", WithNAllocations(uint64(unsafe.Sizeof(v)), func(t *testing.T) {
+	t.Run("with allocation", WithNAllocations(1, func(t *testing.T) {
+		v := make([]byte, 10)
 		v = append(v, 1)
 	}))
 }
